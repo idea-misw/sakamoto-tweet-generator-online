84b047728a39eb9d9cc0e5d585defe7b6cfb3c80
idea-misw <1iota4delta3epsilon3alpha9@gmail.com>
2022-02-13 18:51:44 +0900

    Updates

---
+++ precache-manifest.288c669e0d4aca10513bc79496808414.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c6c49bca5e0b4d9711c7d2b52e326d7",
+    "url": "/sakamoto-tweet-generator-online/index.html"
+  },
+  {
+    "revision": "15c885e7085f01b310fc",
+    "url": "/sakamoto-tweet-generator-online/static/css/main.2fd583e4.chunk.css"
+  },
+  {
+    "revision": "32184c7f8b78a46bde26",
+    "url": "/sakamoto-tweet-generator-online/static/js/2.bb022b47.chunk.js"
+  },
+  {
+    "revision": "b1a8b2d9f786f5873ee5",
+    "url": "/sakamoto-tweet-generator-online/static/js/3.a3f1d22b.chunk.js"
+  },
+  {
+    "revision": "15c885e7085f01b310fc",
+    "url": "/sakamoto-tweet-generator-online/static/js/main.70ddbf3e.chunk.js"
+  },
+  {
+    "revision": "0b624b5e053419dfbd3c",
+    "url": "/sakamoto-tweet-generator-online/static/js/runtime~main.05221029.js"
+  }
+]);
